b07b7a4733a8d40c4e46736791351c245da770bf
Daan Schoenaers <daan.schoenaers@student.ucll.be>
2025-12-23 10:59:47 +0100

    user handling

---
+++ utils/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://acmctsiuvvhusxkwxfil.supabase.co";
+const supabasePublishableKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjbWN0c2l1dnZodXN4a3d4ZmlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzOTgzNjIsImV4cCI6MjA4MTk3NDM2Mn0.Be9AtWOS69ktfsiCQe8hU_AkEqS57ZYDd0qTBD9keME";
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
