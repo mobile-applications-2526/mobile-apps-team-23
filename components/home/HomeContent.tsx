f3cc2a5d7de71657bd4baf91d90f41231459e432
Dee419 <99879410+Dee419@users.noreply.github.com>
2025-12-29 19:48:28 +0100

    Use === instead of === in HomeContent.tsx

--- components/home/HomeContent.tsx
+++ components/home/HomeContent.tsx
@@ -19,14 +19,14 @@ export default function HomeContent({ router }: { router: Router }) {
   return (
     <>
       <HomeHeader homeStatus={homeStatus} setHomeStatus={setHomeStatus} />
-      {homeStatus == HomeStatus.FRIENDS && (
+      {homeStatus === HomeStatus.FRIENDS && (
         <HomeFriendContent router={router} />
       )}
-      {homeStatus == HomeStatus.LIVE_MAP && <HomeMapContent />}
-      {homeStatus == HomeStatus.TIMELINE && (
+      {homeStatus === HomeStatus.LIVE_MAP && <HomeMapContent />}
+      {homeStatus === HomeStatus.TIMELINE && (
         <HomeTimelineContent router={router} />
       )}
-      {homeStatus == HomeStatus.SETTINGS && <HomeSettingsContent />}
+      {homeStatus === HomeStatus.SETTINGS && <HomeSettingsContent />}
     </>
   );
 }
